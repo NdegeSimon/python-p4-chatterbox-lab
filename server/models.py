ecc0ec47ac5f44e34acec229cb19e689a7676943
SimonNdege <simopetley@gmail.com>
2025-09-24 22:49:12 +0300

    add changes

--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, Column,Integer, String, DateTime, Float, ForeignKey
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.sql import func
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -12,3 +13,7 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(500), nullable=False)
+    username = db.Column(db.String(50), nullable = False)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
